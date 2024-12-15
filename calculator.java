09e708bcfe61a89fcee9fbce5b1b7bbb32db8b45
faridpatel99 <patelfarid81@gmail.com>
2024-12-15 16:06:11 +0530

    project

---
+++ calculator.java
@@ -0,0 +1,69 @@
+<!Doctype html>
+<html lang = "en">
+<head>
+  <titlt><calculator></titlt>
+  <link rel= "stylesheet" href = "style.css">
+  </head>
+  <body>
+  <div id = "calculator">
+  <div id = "content">
+  <form>
+  <div id = "output">
+<input type = "text" id = 'res'>
+</div>
+<div class = "btn"
+ <inputtype="button" value='c'>
+oneclick="clear()" id="clear">
+ <input type="button" value='-'
+ oneclick= ""Back()" id ="backspace">
+ <input type= "button" value='%'
+ oneclick="solve('%')">
+ <input type= "button" value='%'oneclick="solve('%')">
+ <br>
+ <input type= "button" value='7'oneclick="solve('7')">
+ <input type= "button" value='8'oneclick="solve('8')">
+ <input type= "button" value='9'oneclick="solve('9')">
+ <input type= "button" value='x'oneclick="solve('*')">
+ <br>
+ <input type="button"value='4' oneclick=solve('4')">
+ <input type="button"value='5' oneclick=solve('5')">
+ <input type="button"value='6' oneclick=solve('6')">
+ <input type="button"value='-' oneclick=solve('-')">
+ <br>
+ <input type="button" value='1' oneclick="solve('1')">
+ <input type="button" value='2' oneclick="solve('2')">
+ <input type="button" value='3' oneclick="solve('3')">
+ <input type="button" value='+' oneclick="solve('+')">
+ <br>
+ <input type="button" value='00' oneclick="solve('00')">
+ <input type="button" value='0' oneclick="solve('0')">
+ <input type="button" value='.' oneclick="solve('.')">
+ <input type="button" value='=' oneclick="Result()">
+ </div>
+ </form>
+ </div>
+ </div>
+ <script src='index.js'></script>
+ </body>
+ </html>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
